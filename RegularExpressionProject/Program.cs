e21f1430ee1a2f461786c1e3e42e2fc0b16cc409
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-05 17:47:25 +0300

    Добавьте файлы проекта.

---
+++ RegularExpressionProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "aaaaaaa bbbbbb aabbss fffdddaaa";
+
+            // (111) 123-45-67
+            Regex regex = new Regex(@"(^\w*|\s+|^)a{2}\w*");  //(@"\(\d{3}\)\s*\d{3}-\d{2}-\d{2}");
+
+            //if (regex.IsMatch(str))
+            //    Console.WriteLine("phone!");
+            //else
+            //    Console.WriteLine("not phone!");
+
+            MatchCollection matches = regex.Matches(str);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+
+
+            //string strRepl = regex.Replace(str, "qq");
+            //Console.WriteLine(strRepl);
+        }
+    }
+}
